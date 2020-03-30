3dee59ae607d505d6ba3b509ade99aabe31d2572
Moppler Code <findmoppler@gmail.com>
2020-03-30 11:59:42 +0000

    Add a message alerting users that a critter is not available next month.

--- app/components/critter-table-row.js
+++ app/components/critter-table-row.js
@@ -1,5 +1,19 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  tagName: 'tr'
+  tagName: 'tbody',
+  // classNameBindings: ['expiresSoon:table-warning'],
+  
+  expiresSoon: computed('critter', 'currentTime.month', 'hemisphere', function() {
+    let nextMonth = this.get('currentTime.month') + 1;
+    if(nextMonth > 11) {
+      nextMonth = 0;
+    }
+
+    if(!this.get('critter.months')[this.get('hemisphere')].includes(nextMonth)) {
+      return true;
+    }
+    return false;
+  })
 });
