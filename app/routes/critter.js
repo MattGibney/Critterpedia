db74e25fecca7dc1c4054a86aed03f3f81ef1b4d
Moppler Code <findmoppler@gmail.com>
2020-04-01 21:49:58 +0000

    Added og:tags to the critter page

--- app/routes/critter.js
+++ app/routes/critter.js
@@ -10,35 +10,48 @@ export default Route.extend({
   },
 
   setHeadTags(model) {
-    let headTags = [{
-      type: 'meta',
-      tagId: 'meta-og-title',
-      attrs: {
-        property: 'og:title',
-        content: `Critterpedia | ${model.name}`
+    let headTags = [
+      {
+        type: 'meta',
+        tagId: 'meta-og-title',
+        attrs: {
+          property: 'og:title',
+          content: `Critterpedia | ${model.name}`
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-image',
+        attrs: {
+          property: 'og:image',
+          content: `https://critterpedia.moppler.co.uk/assets/app-icon.png`
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-url',
+        attrs: {
+          property: 'og:url',
+          content: `https://critterpedia.moppler.co.uk/critter/${encodeURIComponent(model.name)}`
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-type',
+        attrs: {
+          property: 'og:type',
+          content: 'website'
+        }
+      },
+      {
+        type: 'link',
+        tagId: 'canonical-link',
+        attrs: {
+          rel: 'canonical',
+          href: `https://critterpedia.moppler.co.uk/critter/${encodeURIComponent(model.name)}`
+        }
       }
-    },{
-      type: 'meta',
-      tagId: 'meta-og-url',
-      attrs: {
-        property: 'og:url',
-        content: `https://critterpedia.moppler.co.uk/critter/${encodeURIComponent(model.name)}`
-      }
-    },{
-      type: 'meta',
-      tagId: 'meta-og-type',
-      attrs: {
-        property: 'og:type',
-        content: 'website'
-      }
-    },{
-      type: 'link',
-      tagId: 'canonical-link',
-      attrs: {
-        rel: 'canonical',
-        href: `https://critterpedia.moppler.co.uk/critter/${encodeURIComponent(model.name)}`
-      }
-    }];
+    ];
 
     this.set('headTags', headTags);
   }
