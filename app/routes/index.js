8b543d52a5682b0377960a15ffd8c1eb5e92862a
Moppler Code <findmoppler@gmail.com>
2020-04-04 19:07:56 +0000

    Added meta information to the index page, in an attempt to improve search rankings on Google.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,54 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  afterModel() {
+    this.setHeadTags();
+  },
+
+  setHeadTags() {
+    let headTags = [
+      {
+        type: 'meta',
+        tagId: 'meta-og-image',
+        attrs: {
+          property: 'og:image',
+          content: 'https://critterpedia.moppler.co.uk/assets/app-icon.png'
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-description',
+        attrs: {
+          name: 'description',
+          content: `Animal Crossing New Horizons companion app. Perfect for the mobile, this site provides all of the information you need for catching Fish and Insects. It's your very own Critterpedia, right in your pocket!`
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-url',
+        attrs: {
+          property: 'og:url',
+          content: 'https://critterpedia.moppler.co.uk/'
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-type',
+        attrs: {
+          property: 'og:type',
+          content: 'website'
+        }
+      },
+      {
+        type: 'link',
+        tagId: 'canonical-link',
+        attrs: {
+          rel: 'canonical',
+          href: 'https://critterpedia.moppler.co.uk/'
+        }
+      }
+    ];
+
+    this.set('headTags', headTags);
+  }
 });
