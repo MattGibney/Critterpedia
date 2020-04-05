ab2de28ec130dd6718274cd03506ac383a377fee
Moppler <findmoppler@gmail.com>
2020-04-05 12:52:36 +0100

    Fix icons missing from production

--- config/icons.js
+++ config/icons.js
@@ -1,14 +1,26 @@
+/**
+ * Font Awesome icon settings
+ * At build time, this file is used to create a custom fontawesome font file
+ * containing only the icons that are required for the application. This file
+ * dramatically reduces the size of the compiled application.
+ * 
+ * Any new icons that are required, also need to be added to this file.
+ */
+
 module.exports = function() {
   return {
     'free-brands-svg-icons': [
       'twitter'
     ],
     'pro-solid-svg-icons': [
+      'chevron-left',
       'cog',
       'sort',
       'sort-down',
       'sort-up',
-      'badge-check'
+      'badge-check',
+      'exclamation-triangle',
+      'info-circle'
     ]
   };
 };
