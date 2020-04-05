6d7e03de0145a2c1916a206c1ef306a284ad2d58
Moppler Code <findmoppler@gmail.com>
2020-04-05 10:16:18 +0000

    Remove un-used deps. subset icons to reduce over all payload

---
+++ config/icons.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+  return {
+    'free-brands-svg-icons': [
+      'twitter'
+    ],
+    'pro-solid-svg-icons': [
+      'cog',
+      'sort',
+      'sort-down',
+      'sort-up',
+      'badge-check'
+    ]
+  };
+};
